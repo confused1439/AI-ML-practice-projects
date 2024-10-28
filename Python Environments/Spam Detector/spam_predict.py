f71b8374bd9135a8a76c3d2589e73db885ca85b8
confused1439 <saradvavivek@gmail.com>
2024-10-28 13:10:13 +0530

    Added the Spam Detector folder

---
+++ Python Environments/Spam Detector/spam_predict.py
@@ -0,0 +1,17 @@
+import joblib, sys
+
+def load_model():
+    model = joblib.load('spam_detector_model.pkl')
+    vectorizer = joblib.load('spam_detector_vectorizer.pkl')
+    return model, vectorizer
+
+def predict(text):
+    model, vectorizer = load_model()
+    text_transformed = vectorizer.transform([text])
+    prediction = model.predict(text_transformed)
+    return "Spam" if prediction[0] == 1 else "Not a spam"
+
+if __name__ == "__main__":
+    text = sys.argv[1]
+    prediction = predict(text)
+    print(f"The message is: {prediction}")
